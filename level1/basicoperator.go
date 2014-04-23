16c67cfdec98f8df6e4ab989de526048d234de25
anandaverma <rahul.vit09@gmail.com>
2014-04-23 17:53:20 +0530

    basicoperator

---
+++ level1/basicoperator.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func main() {
+	fmt.Println("2+1=", 2+1);
+	fmt.Println("2-1=", 2-1)
+	fmt.Println("2*2=", 2*2)
+	fmt.Println("4/3=", 4/3)
+	fmt.Println("4.0/3.0=", 4.0/3.0)
+}
